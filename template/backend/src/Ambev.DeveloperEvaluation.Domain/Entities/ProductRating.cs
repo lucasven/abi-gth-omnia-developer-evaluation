711b46452c2024fc2e468a4bf78f33503a64dbd9
Lucas Venturella <lucas.venturella@hotmail.com>
2024-12-17 02:19:41 -0300

    separated productrating to another class

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/ProductRating.cs
@@ -0,0 +1,12 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Common;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities
+{
+    public class ProductRating : BaseEntity
+    {
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }
+        public decimal RateValue { get; set; }
+        public int Count { get; set; }
+    }
+} 
